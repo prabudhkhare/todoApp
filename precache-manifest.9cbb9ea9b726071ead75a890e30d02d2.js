a5240d5964fcbce0b81b90cc4da50dc132bc4428
prabudhkhare <khareprabudh2@gmail.com>
2020-09-12 17:25:01 +0530

    Updates

---
+++ precache-manifest.9cbb9ea9b726071ead75a890e30d02d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843b727f33c5d73da7df3675e9184989",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "ddf1fb650e9aad9b45d5",
+    "url": "/todoApp/static/css/main.c3e03687.chunk.css"
+  },
+  {
+    "revision": "b32788187397f4fdb0d9",
+    "url": "/todoApp/static/js/2.5f6828a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.5f6828a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf1fb650e9aad9b45d5",
+    "url": "/todoApp/static/js/main.fef407bf.chunk.js"
+  },
+  {
+    "revision": "176b111f644b8b8c0526",
+    "url": "/todoApp/static/js/runtime-main.1e560017.js"
+  }
+]);
