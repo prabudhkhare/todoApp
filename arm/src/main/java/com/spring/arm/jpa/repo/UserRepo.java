0cb7f8538abc546a05c5d8ec638a92f78fcc1cfa
prabudhkhare <71174569+prabudhkhare@users.noreply.github.com>
2021-10-12 10:19:37 +0530

    Delete arm directory

--- arm/src/main/java/com/spring/arm/jpa/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.spring.arm.jpa.repo;
-
-import com.spring.arm.jpa.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepo extends CrudRepository<User,String> {
-}
--- arm/src/main/java/com/spring/arm/model/security/Provider.java
+++
@@ -1,5 +0,0 @@
-package com.spring.arm.model.security;
-
-public enum Provider {
-    GOOGLE,FACEBOOK,LOCAL
-}
